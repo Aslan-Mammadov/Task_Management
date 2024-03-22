88eafefef262821f9ac346aefaf6ef255577b6b2
Aslan <aslan.mammadov96@gmail.com>
2024-03-22 19:44:04 -0400

    add Contacts form

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
 
 function Contacts() {
   return (
-    <div>
-      Contacts
-    </div>
-  )
+    <>
+      <form className="contacts-form">
+        <h1>Add New Contact</h1>
+        <label htmlFor="name">First Name</label>
+        <input type="text" name="name" />
+
+        <label htmlFor="last-name">Last Name</label>
+        <input type="text" name="last-name" />
+
+        <label for="email">
+          Email
+          <input type="email" name="email" />
+        </label>
+
+        <label htmlFor="phone">
+          Phone Number
+          <input type="cell" name="phone" />
+        </label>
+
+        <label for="comments">
+          Comments
+          <textarea cols="70" rows="7" placeholder="optional" name="comments" />
+        </label>
+
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
 
-export default Contacts
+export default Contacts;
--- src/components/TaskManager.jsx
+++ src/components/TaskManager.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Tasks from "./Tasks";
 
 function TaskManager() {
-  const [task, setTask] = useState('');
+  const [task, setTask] = useState("");
   const [savedTasks, setSavedTasks] = useState([]);
 
   const handleChange = (e) => {
@@ -24,19 +24,18 @@ function TaskManager() {
 
   return (
     <>
-    
-    <form onSubmit={handleSubmit}>
-    <h1>Daily Task Management</h1>
-      <input
-        type="text"
-        value={task}
-        placeholder="Task"
-        onChange={handleChange}
-        required
-      />
-      <button type="submit">Add Task</button>
-    </form>
-     <Tasks savedTasks={savedTasks} handleRemove={handleRemove}/>
+      <form onSubmit={handleSubmit}>
+        <h1>Daily Task Management</h1>
+        <input
+          type="text"
+          value={task}
+          placeholder="Task"
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Add Task</button>
+      </form>
+      <Tasks savedTasks={savedTasks} handleRemove={handleRemove} />
     </>
   );
 }
