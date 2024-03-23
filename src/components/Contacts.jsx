97ed9a949b34ff0d4a11cec29f2877b64e915db6
Aslan <aslan.mammadov96@gmail.com>
2024-03-22 20:35:26 -0400

    add css to contacts

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -5,26 +5,20 @@ function Contacts() {
     <>
       <form className="contacts-form">
         <h1>Add New Contact</h1>
-        <label htmlFor="name">First Name</label>
+        <label htmlFor="name">First Name</label> 
         <input type="text" name="name" />
 
-        <label htmlFor="last-name">Last Name</label>
+        <label htmlFor="last-name">Last Name</label> 
         <input type="text" name="last-name" />
 
-        <label for="email">
-          Email
+        <label htmlFor="email">Email</label>  
           <input type="email" name="email" />
-        </label>
 
-        <label htmlFor="phone">
-          Phone Number
+        <label htmlFor="phone">Phone Number</label>  
           <input type="cell" name="phone" />
-        </label>
 
-        <label for="comments">
-          Comments
-          <textarea cols="70" rows="7" placeholder="optional" name="comments" />
-        </label>
+        <label htmlFor="comments">Comments:</label>  
+          <textarea cols="50" rows="5" placeholder="optional" name="comments" />
 
         <button type="submit">Submit</button>
       </form>
