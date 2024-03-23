8bfd8d49b10f0aa5bf09982fa36adc2a0aa06f37
Aslan <aslan.mammadov96@gmail.com>
2024-03-22 20:52:07 -0400

    clean up code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { useState } from 'react';
-import './App.css';
-import Navigation from './components/Navigation';
-import Container from './Container';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import TaskManager from "./components/TaskManager";
+import Contacts from "./components/Contacts";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <div className="App">
-    <Navigation/>
-    <Container/>
-    </div>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<TaskManager />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </>
   );
 }
 
